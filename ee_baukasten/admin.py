00a15231a5b161da20c02651364fd61947ee033e
Amsel <>
2015-08-09 21:05:08 +0200

    impement example models
    - just to get warmed up

--- ee_baukasten/admin.py
+++ ee_baukasten/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Spieler)
+admin.site.register(models.EigenEntwicklung)
+admin.site.register(models.Anwendungsgebiet)
+admin.site.register(models.Effekt)
--- ee_baukasten/models.py
+++ ee_baukasten/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Spieler(models.Model):
+    name = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+
+class EigenEntwicklung(models.Model):
+    name = models.CharField(max_length=127)
+    ersteller = models.ForeignKey(Spieler)
+
+    def __str__(self):
+        return self.name
+
+
+class Anwendungsgebiet(models.Model):
+    name = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+
+class Effekt(models.Model):
+    name = models.CharField(max_length=127)
+    description = models.TextField()
+    entwicklungen = models.ManyToManyField(EigenEntwicklung)
+    anwendungsgebiet = models.ForeignKey(Anwendungsgebiet)
+
+    def __str__(self):
+        return self.name
+
+
+
