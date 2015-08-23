6b6be8ca52dc659d4fd13816d80cf843d6ecbec6
Amsel <>
2015-08-23 14:09:54 +0200

    beginne experimentelle Seite für Effektgruppe

--- ee_baukasten/views.py
+++ ee_baukasten/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import EigenEntwicklung, Effektgruppe
+from .models import EigenEntwicklung, Effekt, Effektgruppe
 
 # Create your views here.
 
@@ -16,3 +16,9 @@ def ee_detail(request, ee_id):
     ee = get_object_or_404(EigenEntwicklung, pk=ee_id)
     effekte = get_list_or_404(ee.effekte.all())
     return render(request, template_name, {'ee': ee, 'effekte': effekte})
+
+
+def effekte_in_gruppe(request, gruppe):
+    template_name = 'group_list.html'
+    effekte = get_list_or_404(Effekt, gruppe=gruppe)
+    return render(request, template_name, {'effekte': effekte})
