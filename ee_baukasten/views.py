6dfd4954908c5078f9bd2653de924348689bc5fc
Amsel <>
2015-08-23 13:51:46 +0200

    füge Effektgruppen zum index hinzu und EE-Art zu den details

--- ee_baukasten/views.py
+++ ee_baukasten/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import EigenEntwicklung
+from .models import EigenEntwicklung, Effektgruppe
 
 # Create your views here.
 
 
 def index(request):
     template_name = 'index.html'
-    ees = EigenEntwicklung.objects.order_by('name')
-    return render(request, template_name, {'ee_list': ees})
+    ees = get_list_or_404(EigenEntwicklung.objects.order_by('name'))
+    gruppen = get_list_or_404(Effektgruppe.objects.all())
+    return render(request, template_name, {'ee_list': ees, 'gruppen': gruppen})
 
 
 def ee_detail(request, ee_id):
