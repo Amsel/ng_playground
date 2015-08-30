b1994d15a146d6cfd62d339be04f2b84ed71aa53
Amsel <>
2015-08-30 11:37:24 +0200

    cleanup
    - formatierung
    - nutze field_set um teilmenge der m2n Beziehung zu holen

--- ee_baukasten/views.py
+++ ee_baukasten/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import EigenEntwicklung, Effekt, Effektgruppe
+from .models import EigenEntwicklung, Effektgruppe
 
 # Create your views here.
 
@@ -8,18 +8,25 @@ def index(request):
     template_name = 'index.html'
     ees = get_list_or_404(EigenEntwicklung.objects.order_by('name'))
     gruppen = get_list_or_404(Effektgruppe.objects.all())
-    return render(request, template_name, {'ee_list': ees, 'gruppen': gruppen})
+    return render(request,
+                  template_name,
+                  {'ee_list': ees, 'gruppen': gruppen})
 
 
 def ee_detail(request, ee_id):
     template_name = 'ee_detail.html'
     ee = get_object_or_404(EigenEntwicklung, pk=ee_id)
     effekte = get_list_or_404(ee.effekte.all())
-    return render(request, template_name, {'ee': ee, 'effekte': effekte})
+    return render(request,
+                  template_name,
+                  {'ee': ee, 'effekte': effekte})
 
 
 def effektgruppe(request, gruppe):
     template_name = 'effektgruppe.html'
     effekt_gruppe = get_object_or_404(Effektgruppe, pk=gruppe)
-    effekte = get_list_or_404(Effekt.objects.filter(gruppe=gruppe))
-    return render(request, template_name, {'gruppe': effekt_gruppe, 'effekte': effekte})
+    # effekt_set bildet die umgekehrte Richtung der m2n Beziehung ab
+    effekte = get_list_or_404(effekt_gruppe.effekt_set)
+    return render(request,
+                  template_name,
+                  {'gruppe': effekt_gruppe, 'effekte': effekte})
